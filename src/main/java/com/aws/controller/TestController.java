c46003ffea73a5527275814b6b5f5eb7377a140b
Hoang Anh <AnhDH5@fsoft.com.vn>
2017-12-26 18:52:58 +0700

    Initial project.

---
+++ src/main/java/com/aws/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.aws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping(value = "/custom", method = RequestMethod.POST)
+    public String custom() {
+        return "custom";
+    }
+}
